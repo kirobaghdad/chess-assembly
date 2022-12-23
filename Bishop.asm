5962d95ef64f74d19b6683a7a188c33059b7d271
Walid <walid.osama.khamees@gmail.com>
2022-12-23 03:51:03 +0200

    generalized form, fixed know bug

--- Bishop.asm
+++ Bishop.asm
@@ -44,14 +44,19 @@ pop bx
 pop ax 
 endm popAlls
 
+loopDiagonally macro
+
+
+endm loopDiagonally
+
 .model large
 .stack 64
 .data
-grid db "br","bn","--","bk","bq","bb","bn","br"
+grid db "br","bn","bb","bk","bq","bb","bn","br"
      db "bp","bp","bp","bp","bp","bp","bp","bp"                                    
      db "--","--","--","--","--","--","--","--"
+     db "--","--","--","--","--","--","--","wb"
      db "--","--","--","--","--","--","--","--"
-     db "--","--","--","bb","--","--","--","--"
      db "--","--","--","--","--","--","--","--"
      db "wp","wp","wp","wp","wp","wp","wp","wp"  
      db "wr","wn","wb","wk","wq","wb","wn","wr"
@@ -75,9 +80,10 @@ bishopMoves proc
 
 
 
-mov ah, 5h
-mov al, 4h
-
+mov ah, 4h
+mov al, 8h
+dec ah
+dec al
 
 mov dx,ax
 
@@ -89,12 +95,22 @@ convertToTile ax
 
 
 cmp grid[bx], "w"
-jz diagonalRightUp
-jmp near ptr black
+je whiteBishop
+jmp blackBishop
 
 
 
 
+whiteBishop:
+mov dl,"w"
+mov dh,"b"
+jmp bishopBegin
+blackBishop:
+mov dl,"b"
+mov dh,"w"
+
+bishopBegin:
+pushAll
 ; decremnt row, increment col
 ; convert to tile 
 ; if it's empty and not out of bound 
@@ -103,47 +119,40 @@ diagonalRightUp:
 dec ah
 inc al
 convertToTile ax
-; if black
-cmp grid[bx], "b"
-jz diagonalRightUpLast
+cmp grid[bx], dh
+je LastdiagonalRightUp
 ; if empty
 cmp grid[bx], "-"
 jnz diagonalLeftUpA
 ; out of bound
-cmp bx, 128D
+cmp ah, 7h
 ja diagonalLeftUpA
+cmp ah, 0h
+jb diagonalLeftUpA
 ; out of bound
-cmp bx, 0D
+cmp al, 7h     
+ja diagonalLeftUpA
+cmp al, 0h
 jb diagonalLeftUpA
 ; add to moves 
-diagonalRightUpLast:
-push bx
-push cx
+LastdiagonalRightUp:
+push bx ; moves
+push cx ; actual move 
 mov cx, 0
-
 mov cx, ax
-
 mov bh, 0
 mov bl, count
-push cx
-push dx
-inc dl 
-inc dh
 inc cl
 inc ch
-mov moves[bx], dx
-mov moves[bx + 2], cx
-pop dx
-pop cx
-add count, 4
-
+mov moves[bx], cx
 pop cx
 pop bx
-cmp grid[bx],"b"
-jz diagonalLeftUpA 
+add count, 2
+cmp grid[bx],dh
+jz diagonalLeftUpA
 jmp diagonalRightUp
-
 diagonalLeftUpA:
+
 popAll
 
 pushAll
@@ -156,45 +165,40 @@ diagonalLeftUp:
 dec ah
 dec al
 convertToTile ax
-; if black
-cmp grid[bx], "b"
-jz diagonalLeftUpLast
+cmp grid[bx],dh
+je LastdiagonalLeftUp
 ; if empty
 cmp grid[bx], "-"
 jnz diagonalRightDownA
-; out of bound
-cmp bx, 128D
+
+cmp ah, 7h
 ja diagonalRightDownA
 ; out of bound
-cmp bx, 0D
+cmp ah, 0h
 jb diagonalRightDownA
+
+; out of bound
+cmp al, 7h
+ja diagonalRightDownA
+
+cmp al, 0h
+jb diagonalRightDownA
+
 ; add to moves 
-diagonalLeftUpLast:
-push bx
-push cx
+LastdiagonalLeftUp:
+push bx ; moves
+push cx ; actual move 
 mov cx, 0
-
 mov cx, ax
-
-
-
 mov bh, 0
 mov bl, count
-push cx
-push dx
-inc dl 
-inc dh
 inc cl
 inc ch
-mov moves[bx], dx
-mov moves[bx + 2], cx
-pop dx
-pop cx
-add count, 4
-
+mov moves[bx], cx
 pop cx
 pop bx
-cmp grid[bx],"b"
+add count, 2
+cmp grid[bx],dh
 jz diagonalRightDownA 
 jmp diagonalLeftUp
 
@@ -210,45 +214,36 @@ diagonalRightDown:
 inc ah
 inc al
 convertToTile ax
-; if black
-cmp grid[bx], "b"
-jz diagonalRightDownLast
+cmp grid[bx], dh
+je LastdiagonalRightDown
 ; if empty
 cmp grid[bx], "-"
 jnz diagonalLeftDownA
 ; out of bound
-cmp bx, 128D
+cmp ah, 7h
 ja diagonalLeftDownA
+cmp ah, 0h
+jb diagonalLeftDownA
 ; out of bound
-cmp bx, 0D
+cmp al, 7h
+ja diagonalLeftDownA
+cmp al, 0h
 jb diagonalLeftDownA
 ; add to moves 
-diagonalRightDownLast:
-push bx
-push cx
+LastdiagonalRightDown:
+push bx ; moves
+push cx ; actual move 
 mov cx, 0
-
 mov cx, ax
-
-
-
 mov bh, 0
 mov bl, count
-push cx
-push dx
-inc dl 
-inc dh
 inc cl
 inc ch
-mov moves[bx], dx
-mov moves[bx + 2], cx
-pop dx
-pop cx
-add count, 4
-
+mov moves[bx], cx
 pop cx
 pop bx
-cmp grid[bx],"b"
+add count, 2
+cmp grid[bx],dh
 jz diagonalLeftDownA 
 jmp diagonalRightDown
 
@@ -266,272 +261,44 @@ diagonalLeftDown:
 inc ah
 dec al
 convertToTile ax
-; if black
-cmp grid[bx], "b"
-jz diagonalLeftDownLast
 ; if empty
-cmp grid[bx], "-"
-jz bx1
-jmp near ptr continue
-bx1:
-; out of bound
-cmp bx, 128D ; check if equal works
-jb bbx2
-jmp near ptr continue
-bbx2:
-; out of bound
-cmp bx, 0D ; check if equal works
-ja bbx3
-jmp near ptr continue
-bbx3:
-; add to moves 
-diagonalLeftDownLast:
-push bx
-push cx
-mov cx, 0
+cmp grid[bx], dh
+je LastdiagonalLeftDown
 
-mov cx, ax
-
-
-
-mov bh, 0
-mov bl, count
-push cx
-push dx
-inc dl 
-inc dh
-inc cl
-inc ch
-mov moves[bx], dx
-mov moves[bx + 2], cx
-pop dx
-pop cx
-add count, 4
-
-pop cx
-pop bx
-cmp grid[bx],"b"
-jnz bx4
-jmp near ptr continue
-bx4:
-jmp diagonalLeftDown
-
-; --------------------------------------------black---------------------------------------------
-
-black: 
-
-diagonalRightUpB: 
-dec ah
-inc al
-convertToTile ax
-; if black
-cmp grid[bx], "w"
-jz diagonalRightUpLastB
-; if empty
-cmp grid[bx], "-"
-jnz diagonalLeftUpAB
-; out of bound
-cmp bx, 128D
-ja diagonalLeftUpAB
-; out of bound
-cmp bx, 0D
-jb diagonalLeftUpAB
-; add to moves 
-diagonalRightUpLastB:
-push bx
-push cx
-mov cx, 0
-
-mov cx, ax
-
-mov bh, 0
-mov bl, count
-push cx
-push dx
-inc dl 
-inc dh
-inc cl
-inc ch
-mov moves[bx], dx
-mov moves[bx + 2], cx
-pop dx
-pop cx
-add count, 4
-
-pop cx
-pop bx
-cmp grid[bx],"w"
-jz diagonalLeftUpAB 
-jmp diagonalRightUpB
-
-diagonalLeftUpAB:
-popAll
-
-pushAll
-
-; decrement row, col
-; convert to tile 
-; if it's empty and not out of bound 
-; add to moves 
-diagonalLeftUpB: 
-dec ah
-dec al
-convertToTile ax
-; if black
-cmp grid[bx], "w"
-jz diagonalLeftUpLastB
-; if empty
-cmp grid[bx], "-"
-jnz diagonalRightDownAB
-; out of bound
-cmp bx, 128D
-ja diagonalRightDownAB
-; out of bound
-cmp bx, 0D
-jb diagonalRightDownAB
-; add to moves 
-diagonalLeftUpLastB:
-push bx
-push cx
-mov cx, 0
-
-mov cx, ax
-
-
-
-mov bh, 0
-mov bl, count
-push cx
-push dx
-inc dl 
-inc dh
-inc cl
-inc ch
-mov moves[bx], dx
-mov moves[bx + 2], cx
-pop dx
-pop cx
-add count, 4
-
-pop cx
-pop bx
-cmp grid[bx],"w"
-jz diagonalRightDownAB 
-jmp diagonalLeftUpB
-
-diagonalRightDownAB:
-popAll
-
-pushAll
-; increment row, col
-; convert to tile 
-; if it's empty and not out of bound 
-; add to moves 
-diagonalRightDownB: 
-inc ah
-inc al
-convertToTile ax
-; if black
-cmp grid[bx], "w"
-jz diagonalRightDownLastB
-; if empty
-cmp grid[bx], "-"
-jnz diagonalLeftDownAB
-; out of bound
-cmp bx, 128D
-ja diagonalLeftDownAB
-; out of bound
-cmp bx, 0D
-jb diagonalLeftDownAB
-; add to moves 
-diagonalRightDownLastB:
-push bx
-push cx
-mov cx, 0
-
-mov cx, ax
-
-
-
-mov bh, 0
-mov bl, count
-push cx
-push dx
-inc dl 
-inc dh
-inc cl
-inc ch
-mov moves[bx], dx
-mov moves[bx + 2], cx
-pop dx
-pop cx
-add count, 4
-
-pop cx
-pop bx
-cmp grid[bx],"w"
-jz diagonalLeftDownAB 
-jmp diagonalRightDownB
-
-diagonalLeftDownAB:
-popAll
-
-
-
-pushAll
-; increment row, col
-; convert to tile 
-; if it's empty and not out of bound 
-; add to moves 
-diagonalLeftDownB: 
-inc ah
-dec al
-convertToTile ax
-; if black
-cmp grid[bx], "w"
-jz diagonalLeftDownLastB
-; if empty
 cmp grid[bx], "-"
 jnz continue
 ; out of bound
-cmp bx, 128D ; check if equal works
+cmp ah, 7h ; check if equal works
 ja continue
-; out of bound
-cmp bx, 0D ; check if equal works
+cmp ah, 0h ; check if equal works
 jb continue
+; out of bound
+cmp al, 7h ; check if equal works
+ja continue
+cmp al, 0h ; check if equal works
+jb continue
+
 ; add to moves 
-diagonalLeftDownLastB:
-push bx
-push cx
+LastdiagonalLeftDown:
+push bx ; moves
+push cx ; actual move 
 mov cx, 0
-
 mov cx, ax
-
-
-
 mov bh, 0
 mov bl, count
-push cx
-push dx
-inc dl 
-inc dh
 inc cl
 inc ch
-mov moves[bx], dx
-mov moves[bx + 2], cx
-pop dx
-pop cx
-add count, 4
-
+mov moves[bx], cx
 pop cx
 pop bx
-cmp grid[bx],"w"
-jz continue 
-jmp diagonalLeftDownB
+add count, 2
+cmp grid[bx],dh
+je continue
+jmp diagonalLeftDown
 
 continue:
 popAll
-
+popAll
 
 ret
 
@@ -549,8 +316,4 @@ hlt
 end start
 
 .end
-
-
-
-
 END
